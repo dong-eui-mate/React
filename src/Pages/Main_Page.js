d31288e8d51e1e5faa7b02a679c55fceb8aec882
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-08-19 04:56:49 +0900

    :art: Footer

--- src/Pages/Main_Page.js
+++ src/Pages/Main_Page.js
@@ -4,8 +4,6 @@ import { Header } from "../layout/Headers_layout";
 import { Nav } from "../layout/Nav_layout";
 
 const Main = () => {
-  getPosWeather();
-    
   return (
     <>
       <Header Title="Main Page" />
--- src/layout/Footer_layout.js
+++ src/layout/Footer_layout.js
@@ -1,5 +1,62 @@
-const Footer = () => {
+import styled from "styled-components";
 
-}
+const Footer = () => {
+  return (
+    <FooterWrap>
+      <FooterColumn>
+        <FooterTitle>팀 이름</FooterTitle>
+        <FooterBody>team DoD</FooterBody>
+        <FooterTitle>서비스 명</FooterTitle>
+        <FooterBody>Deu-mate</FooterBody>
+      </FooterColumn>
+      <FooterColumn>
+        <FooterTitle>팀원</FooterTitle>
+        <FooterBody>김경훈</FooterBody>
+        <FooterBody>김동건</FooterBody>
+        <FooterBody>이승훈</FooterBody>
+        <FooterBody>임도훈</FooterBody>
+      </FooterColumn>
+      <FooterColumn>
+        <FooterTitle>Infomation</FooterTitle>
+        <a href="https://github.com/dong-eui-mate">
+          <FooterBody>GitHub Link</FooterBody>
+        </a>
+        <a href="">
+          <FooterBody>App Download Link</FooterBody>
+        </a>
+        <FooterBody>
+          <div>부산진구 엄광로 176</div>
+          <div>동의대학교 산학관 412호</div>
+        </FooterBody>
+      </FooterColumn>
+    </FooterWrap>
+  );
+};
+
+const FooterWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 30px;
+  padding: 10px 0;
+  border-top: 1px solid gray;
+  width: 350px;
+  justify-content: center;
+`;
+
+const FooterColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 5px 20px;
+`;
+const FooterTitle = styled.div`
+  font-size: 20px;
+  margin: 10px 0;
+`;
+
+const FooterBody = styled.div`
+  font-size: 15px;
+  margin-bottom: 7px;
+`;
 
 export { Footer };
