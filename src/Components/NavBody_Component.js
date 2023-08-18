b84ac17be41bd13a863b30fb9838d45f6707792c
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-08-18 21:44:17 +0900

    :art: menu 변경

--- src/Components/NavBody_Component.js
+++ src/Components/NavBody_Component.js
@@ -2,10 +2,14 @@ import { StyledNavOl } from "../Styles/StyledNavOl";
 import { StyledNavLi } from "../Styles/StyledNavLi";
 
 const NavBody = (props) => {
-  const item = ["Todo", "School Map", "Class"];
-  const link = ["/todo", "/map", "/class"];
+  const item = ["Todo", "Home", "School Map"];
+  const link = ["/todo", "/", "/map"];
   const lis = item.map((_item, index) => {
-    return <StyledNavLi id={index}><a href={link[index]}>{_item}</a></StyledNavLi>;
+    return (
+      <StyledNavLi id={index}>
+        <a href={link[index]}>{_item}</a>
+      </StyledNavLi>
+    );
   });
   return <StyledNavOl>{lis}</StyledNavOl>;
 };
