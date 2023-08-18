8444a5c68b50f7766a5b76445411146f16620a82
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-08-19 04:09:44 +0900

    :art: 위젯 기본 틀

---
+++ src/Components/Widget_Component.js
@@ -0,0 +1,14 @@
+import { StyledWidget } from "../Styles/StyledWidget";
+
+const Widget = () => {
+  return (
+    <StyledWidget>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </StyledWidget>
+  );
+};
+
+export { Widget };
--- src/Pages/Main_Page.js
+++ src/Pages/Main_Page.js
@@ -1,16 +1,17 @@
+import { Widget } from "../Components/Widget_Component";
 import { Footer } from "../layout/Footer_layout";
 import { Header } from "../layout/Headers_layout";
 import { Nav } from "../layout/Nav_layout";
 
 const Main = () => {
-    return (
-        <>
-            <Header Title="Main Page"/>
-            <Nav />
-            Main Page
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Header Title="Main Page" />
+      <Nav />
+      <Widget />
+      <Footer />
+    </>
+  );
+};
 
 export { Main };
